eb988ddda7a924cacf461122070253e1abaa0985
gcomps <gcompitello@gmail.com>
2018-04-19 16:08:44 -0400

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by George Compitello on 4/19/18.
+//  Copyright © 2018 George Compitello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
